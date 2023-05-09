0702ef1665e5ab65a67808251009032677e1526d
yeomchaeeun <codms0654@gmail.com>
2023-05-10 02:39:49 +0900

    Weather-App 2023.05.10 02:39

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
   }
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=0a07842a0bb24bdd9500ea70c6b4236e`
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=0a07842a0bb24bdd9500ea70c6b4236e`
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
