f5f8dae136a5b87ed10ca6a4f3a7979bc652c8b5
yeomchaeeun <codms0654@gmail.com>
2023-05-10 02:42:02 +0900

    Weather-App 2023.05.10 02:42

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -14,7 +14,7 @@ const WeatherButton = ({ cities, setCity, setWeather }) => {
       }
     
       const getWeatherByCurrentLocation = async (lat, lon) => {
-        let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=0a07842a0bb24bdd9500ea70c6b4236e`
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=0a07842a0bb24bdd9500ea70c6b4236e`
         let response = await fetch(url);
         let data = await response.json();
         setWeather(data);
